bf8d12f045cd46271eaaeee97d4543c01372494b
PolytoriaMan <125764413+HurricaneSANS@users.noreply.github.com>
2023-02-19 18:38:49 -0700

    Update users.py

--- users.py
+++ users.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 
 # Loop through profile pages 1-9000
@@ -12,12 +13,18 @@ for profile_number in range(1, 9001):
         # Parse the HTML using Beautiful Soup
         soup = BeautifulSoup(response.content, 'html.parser')
 
+        # Extract the profile ID from the URL
+        profile_id = url.split('/')[-1]
+
         # Find the title tag and extract the username from it
         title_tag = soup.find('title')
         username = title_tag.text.split('|')[0].strip()
 
-        # Write the username to the file
+        # Write the profile ID and username to the file
         with open('users.txt', 'a') as f:
-            f.write(f"{username}\n")
+            f.write(f"ID: {profile_id} Username: {username}\n")
+
+        # Wait for 3 seconds before making the next request
+        time.sleep(3)
     else:
         print(f"Error: Could not retrieve profile {profile_number}")
